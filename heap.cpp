40612b182007a4d9c0a586e08dba7dd3c053e293
Paweł Stawicki <p.stawicki@partner.samsung.com>
2017-09-12 18:45:03 +0200

    heap

---
+++ heap.cpp
@@ -0,0 +1,127 @@
+
+
+#define MAX_SIZE 100000000
+int heap[MAX_SIZE];
+int size = 0;
+
+
+inline void swap(int i , int j){
+    int t = heap[i];
+    heap[i] = heap[j];
+    heap[j] = t;
+}
+
+void up(int start) {
+    while(start > 1 && heap[start/2] > heap[start]) {
+        swap(start/2,start);
+        start /= 2;
+    }
+}
+
+
+void down(int i) {
+    while(i * 2     <= size && heap[i] > heap[i*2] ||
+          i * 2 + 1 <= size && heap[i] > heap[i*2 + 1]){
+        if (i * 2 == size || heap[i * 2] < heap[i*2 + 1]){
+            swap(i, i*2);
+            i = i * 2;
+        } else {
+            swap(i, i*2 + 1);
+            i = i * 2 + 1;
+        }
+    }
+}
+
+
+void put(int e){
+    heap[++size] = e;
+    up(size);
+}
+
+int remove(){
+    int r = heap[1];
+    heap[1] = heap[size--];
+    down(1);
+    return r;
+}
+
+//----------heap sort
+
+void hsort(int *T, int size) {
+    for(int i=0;i<size;i++) put(T[i]);
+    for(int i=0;i<size;i++) T[i]=remove();
+}
+
+
+
+
+
+//--------------BENCHMARK-------------
+#include "benchmark/benchmark.h"
+//#include <cassert>
+#include <algorithm>
+#include <iostream>
+#include <iostream>
+#include <cstdlib>
+
+#define CHECK(x) x
+
+#define ASSERT(x) if(!(x)) std::cerr << "ASSERT " << __PRETTY_FUNCTION__ << " line:" << __LINE__ << " assert: "<< #x << std::endl, std::abort();
+
+
+static void BM_hsort(benchmark::State& state) {
+    while (state.KeepRunning()) {
+        state.PauseTiming();
+        auto n = state.range(0);
+        std::vector<int> v(n), r(n);
+        for(auto &x:v) x = rand();
+        CHECK(
+            auto v2 = v;
+            std::sort(v2.begin(), v2.end());
+        )
+        state.ResumeTiming();
+        hsort(v.data(), v.size());
+        CHECK(
+            state.PauseTiming();
+            ASSERT(std::equal(v.begin(),v.end(),v2.begin()));
+            state.ResumeTiming();
+        )
+    }
+}
+
+
+static void BM_qsort(benchmark::State& state) {
+    while (state.KeepRunning()) {
+        state.PauseTiming();
+        auto n = state.range(0);
+        std::vector<int> v(n), r(n);
+        for(auto &x:v) x = rand();
+        CHECK(
+            auto v2 = v;
+            std::sort(v2.begin(), v2.end());
+        )
+        state.ResumeTiming();
+        std::qsort(v.data(), v.size(), sizeof(int), [](const void* a, const void* b)
+        {
+            int arg1 = *static_cast<const int*>(a);
+            int arg2 = *static_cast<const int*>(b);
+        //  return (arg1 > arg2) - (arg1 < arg2); // possible shortcut
+            return arg1 - arg2; // erroneous shortcut (fails if INT_MIN is present)
+        });
+        CHECK(
+            state.PauseTiming();
+            ASSERT(std::equal(v.begin(),v.end(),v2.begin()));
+            state.ResumeTiming();
+        )
+    }
+}
+
+
+#define BM(x) BENCHMARK(x)->Ranges({{8, 1<<24}})
+
+BM(BM_hsort);
+BM(BM_qsort);
+
+
+
+BENCHMARK_MAIN();

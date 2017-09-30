776161c425adffb186cc744e65e409828949dc6c
Paweł Stawicki <stawel+chealiCharger@gmail.com>
2017-09-30 22:54:42 +0200

    isPrime faster

--- stl/numbers.h
+++ stl/numbers.h
@@ -3,10 +3,11 @@
 
 template<class I>
 bool isPrime(I x) {
-    if (x < 2)
-        return false;
-    for (I i = 2; i * i <= x; i++)
-        if (!(x % i))
+    if (x == 2) return true;
+    if (x < 2 || (x & 1) == 0) return false;
+
+    for (I i = 3; i * i <= x; i+=2) //TODO: i <= sqrt(x) - should be faster
+        if (x % i == 0)
             return false;
     return true;
 }
